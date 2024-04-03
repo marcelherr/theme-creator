6118caad0619c514c0615ea73014b9559aef53fe
Marcel Herr <mrclhrr@gmail.com>
2024-04-03 11:34:50 +0200

    change folder and file structure

---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">Theme Creator</h1>
+    </header>
+  );
+}
