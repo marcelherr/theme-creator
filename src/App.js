85cb48f166fea3db4e91de73c52ac26f4b8a0f92
Marcel Herr <mrclhrr@gmail.com>
2024-04-04 16:31:10 +0200

    optimize App.js

--- src/App.js
+++ src/App.js
@@ -26,3 +26,28 @@ function App() {
 }
 
 export default App;
+
+// import "./App.css";
+
+// import Theme from "./components/Theme";
+
+// function App() {
+//   return (
+//     <>
+//       <header className="header">
+//         <h1>Theme Creator</h1>
+//       </header>
+//       <main className="main-container">
+//         <ul className="theme-list">
+//           {themes.map((theme) => (
+//             <li key={theme.id}>
+//               <Theme name={theme.name} colors={theme.colors} />
+//             </li>
+//           ))}
+//         </ul>
+//       </main>
+//     </>
+//   );
+// }
+
+// export default App;
