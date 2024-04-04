09870947ecc75454acd06690e4e149bc4ebfa172
Marcel Herr <mrclhrr@gmail.com>
2024-04-04 16:30:25 +0200

    add files

--- src/components/ThemeDetail/index.js
+++ src/components/ThemeDetail/index.js
@@ -14,3 +14,18 @@ export default function ThemeDetail({ color }) {
     </article>
   );
 }
+
+// import ColorCard from "./ColorCard";
+// import "./ColorList.css";
+
+// export default function ColorList({ colors }) {
+//   return (
+//     <ul className="color-list">
+//       {colors.map((color) => (
+//         <li key={color.role}>
+//           <ColorCard color={color} />
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// }
