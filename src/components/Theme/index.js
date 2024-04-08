ccb60505dd27ea3649f6fdd17e15f6c911df1c3f
Marcel Herr <mrclhrr@gmail.com>
2024-04-08 16:57:50 +0200

    fixed errors

--- src/components/Theme/index.js
+++ src/components/Theme/index.js
@@ -3,11 +3,19 @@ import { useState } from "react";
 import { IconChevronDown, IconChevronUp } from "@tabler/icons-react";
 import ThemePreview from "../ThemePreview";
 import ThemeList from "../ThemeList";
+import EditThemeForm from "../ThemeEdit";
 
 export default function Theme({ name, colors, onDeleteClick, onEditClick }) {
   const [showDetails, setShowDetails] = useState(false);
+  const [editTheme, setEditTheme] = useState(false);
+
   function toggleDetails() {
     setShowDetails(!showDetails);
+  }
+  function handleEditClick() {
+    setEditTheme(true);
+    onEditClick();
+    setShowDetails(true);
   }
 
   return (
@@ -16,25 +24,33 @@ export default function Theme({ name, colors, onDeleteClick, onEditClick }) {
         <button className="toggle-button" onClick={toggleDetails}>
           <h2 className="theme-title">{name}</h2>
           {showDetails ? (
-            <IconChevronUp size="3rem" />
+            <IconChevronUp size="2rem" />
           ) : (
-            <IconChevronDown size="3rem" />
+            <IconChevronDown size="2rem" />
           )}
         </button>
         {showDetails ? (
           <div>
-            <button
-              type="button"
-              className="delete-button"
-              onClick={onDeleteClick}
-            >
-              Delete Theme
-            </button>
-            <button className="edit-button" onClick={onEditClick}>
-              Edit Theme
-            </button>
-            <ThemeList colors={colors} />
+            {editTheme ? (
+              <EditThemeForm name={name} colors={colors} />
+            ) : (
+              <div>
+                <button
+                  type="button"
+                  className="delete-button"
+                  onClick={onDeleteClick}
+                >
+                  Delete Theme
+                </button>
+                <button className="edit-button" onClick={handleEditClick}>
+                  Edit Theme
+                </button>
+                <ThemeList colors={colors} />
+              </div>
+            )}
           </div>
+        ) : editTheme ? (
+          <EditThemeForm name={name} colors={colors} />
         ) : (
           <ThemePreview colors={colors} />
         )}
